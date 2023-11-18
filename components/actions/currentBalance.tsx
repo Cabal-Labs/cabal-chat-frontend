bd6db20b279cb6401cc948c42351eeb99dbe75b4
Burak Yesil <byesil@signetresearch.com>
2023-11-18 18:26:42 +0300

    added all the ui formatting and made the ui nicer

--- components/actions/currentBalance.tsx
+++ components/actions/currentBalance.tsx
@@ -1,31 +1,25 @@
 export type CurrentBalanceType = {
-	type: "action";
-	action: "current-balance";
-	chain: string;
-	amount: number;
-	currency: string;
+    type: "action";
+    action: "current-balance";
+    chain: string;
+    amount: number;
+    currency: string;
 };
 
-export default function CurrentBalance(message: CurrentBalanceType) {
-	return (
-		<div className='px-4 py-2 rounded bg-yellow-100'>
-			<b>Your Current Balance: </b>
-
-			{/* Data container */}
-			<div className='flex justify-between items-center mt-4'>
-				{/* From data */}
-				<div>
-					<p>
-						Blockchain: <span className='text-blue-500'>{message.chain}</span>
-					</p>
-					<p>
-						Current Balance:{" "}
-						<span className='text-blue-500'>
-							{message.amount} {message.currency}
-						</span>
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+export default function CurrentBalance({ chain, amount, currency }) {
+    return (
+        <div className="p-4 bg-yellow-100 rounded-lg shadow-md flex justify-between items-center">
+            <div>
+                <div className="font-bold text-lg">Bitcoin</div>
+                <div className="text-gray-800 text-sm">{chain}</div>
+            </div>
+            <div className="text-right">
+                <div className="font-bold text-orange-500 text-2xl">
+                    {amount.toLocaleString()} {currency}
+                </div>
+                <div className="text-green-500 font-bold">+1.2%</div>
+            </div>
+            {/* ToDo add images of graph once we figure out graph representation*/}
+        </div>
+    );
 }
